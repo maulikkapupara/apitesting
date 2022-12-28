b8d882129680c27d4634195c6d5e379203c060d6
maulikkapupara <maulikkapupara2805@gmail.com>
2022-12-28 18:47:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const getquotes = async () => {
+    const fetchdata = await fetch(`https://picsum.photos/`);
+    const response = await fetchdata.json();
+    console.log(response);
+  };
+  return (
+    <div className="App">
+      <button onClick={getquotes}>click</button>
+    </div>
+  );
+}
+
+export default App;
